0236615b3ee9357488d14889acad548e0029d5c3
Allison <allison.hix86@gmail.com>
2018-08-16 17:10:47 -0500

    .

--- CG 6-2/Program.cs
+++ CG 6-2/Program.cs
@@ -6,7 +6,16 @@ namespace CG_6_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask user for sentence
+            Console.Write("Please write a sentence: ");
+            
+            // convert user input into variable to call on later
+            var mySentence = Console.ReadLine();
+            
+            // call on the variable and write it in all caps with ToUpper
+            // put the user sentence on a new line under my message by using backslash n for new line :) 
+            Console.WriteLine("TA-DA" + " now your sentece is all uppercase\n" + mySentence.ToUpper());
+            Console.ReadLine();
         }
     }
 }
